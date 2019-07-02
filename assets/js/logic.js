f2a59bead9b4e01ee4aedb36081ba764fbad1b2b
andypande <andypande@gmail.com>
2019-07-02 15:12:43 -0500

    Almost final

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -18,16 +18,23 @@ function debounce(func, wait = 20, immediate = true) {
   function checkSlide() {
     sliderImages.forEach(sliderImage => {
       // half way through the image
-      const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+      const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.clientHeight / 2;
       // bottom of the image
-      const imageBottom = sliderImage.offsetTop + sliderImage.height;
+      const imageBottom = sliderImage.offsetTop + sliderImage.clientHeight;
       const isHalfShown = slideInAt > sliderImage.offsetTop;
       const isNotScrolledPast = window.scrollY < imageBottom;
       if (isHalfShown && isNotScrolledPast) {
-        sliderImage.classList.add('active');
-      } else {
-        sliderImage.classList.remove('active');
-      }
+        var element = document.getElementById("lightDarkSection");
+        element.classList.remove('old-class');
+        element.classList.add("new-class");
+        sliderImage.classList.add('scale-up-hor-left');
+      } 
+    //   else {
+    //     var element = document.getElementById("lightDarkSection");
+    //     element.classList.remove('new-class');
+    //     element.classList.add("old-class");        
+    //     sliderImage.classList.remove('scale-up-hor-left');
+    //   }
     });
   }
 
